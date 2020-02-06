2a9c87d901eb9ad1a2a488939a3614237a1c415e
Victor Huynh <45438361+victor-h-huynh@users.noreply.github.com>
2020-02-06 15:50:13 -0500

    render jsx
    -rendering jsx.

---
+++ indecision-app/public/scripts/app.js
@@ -0,0 +1,11 @@
+console.log('App.js is running!')
+
+// JSX - JavaScript XML
+var template = React.createElement(
+  "h1",
+  { id: "someid" },
+  "Something new"
+)
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
